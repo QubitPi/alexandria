d0d453ba423c7ae182a3bd3e55d43c7adf9fc17d
Jack <jack20220723@gmail.com>
2024-11-27 11:02:14 +0800

    PoC landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Dashboard from "./components/dashboard/Dashboard";
+import Audios from "./components/audios/Audios";
 
 function App() {
   return (
@@ -8,41 +9,7 @@ function App() {
         <main>
           <section className="glass">
             <Dashboard />
-            <div className="audios">
-              <div className="status">
-                <h1>Aristotle</h1>
-                <input type="text"/>
-              </div>
-              <div className="cards">
-                <div className="card">
-                  <img src="images/first.png" width="10%" alt=""/>
-                  <div className="card-info">
-                    <h2>Categories</h2>
-                    <p>Aristotle's Six Works on Logics</p>
-                    <div className="progress"></div>
-                  </div>
-                  <h2 className="percentage">10%</h2>
-                </div>
-                <div className="card">
-                  <img src="images/second.png" width="10%" alt=""/>
-                  <div className="card-info">
-                    <h2>On Interpretation</h2>
-                    <p>Aristotle's Six Works on Logics</p>
-                    <div className="progress"></div>
-                  </div>
-                  <h2 className="percentage">0%</h2>
-                </div>
-                <div className="card">
-                  <img src="images/third.png" width="10%" alt=""/>
-                  <div className="card-info">
-                    <h2>Prior Analytics</h2>
-                    <p>Aristotle's Six Works on Logics</p>
-                    <div className="progress"></div>
-                  </div>
-                  <h2 className="percentage">0%</h2>
-                </div>
-              </div>
-            </div>
+            <Audios />
           </section>
         </main>
         <div className="circle1"></div>
---
+++ src/components/audios/Audios.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './Audios.css';
+
+function Audios() {
+  return (
+      <div className="audios">
+        <div className="status">
+          <h1>Aristotle</h1>
+          <input type="text"/>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <img src="images/first.png" width="10%" alt=""/>
+            <div className="card-info">
+              <h2>Categories</h2>
+              <p>Aristotle's Six Works on Logics</p>
+              <div className="progress"></div>
+            </div>
+            <h2 className="percentage">10%</h2>
+          </div>
+          <div className="card">
+            <img src="images/second.png" width="10%" alt=""/>
+            <div className="card-info">
+              <h2>On Interpretation</h2>
+              <p>Aristotle's Six Works on Logics</p>
+              <div className="progress"></div>
+            </div>
+            <h2 className="percentage">0%</h2>
+          </div>
+          <div className="card">
+            <img src="images/third.png" width="10%" alt=""/>
+            <div className="card-info">
+              <h2>Prior Analytics</h2>
+              <p>Aristotle's Six Works on Logics</p>
+              <div className="progress"></div>
+            </div>
+            <h2 className="percentage">0%</h2>
+          </div>
+        </div>
+      </div>
+  );
+}
+
+export default Audios;
