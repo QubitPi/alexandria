cd893d085504167d4296a88becd3913ec370c895
Jack <jack20220723@gmail.com>
2024-11-27 11:00:24 +0800

    PoC landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,13 @@
 import React from 'react';
 import './App.css';
+import Dashboard from "./components/dashboard/Dashboard";
 
 function App() {
   return (
       <>
         <main>
           <section className="glass">
-            <div className="dashboard">
-              <div className="project">
-                <div className="imageCropper">
-                  <img src="./images/avatar.png" alt=""/>
-                </div>
-                <h3>Alexandria</h3>
-                <p>Library</p>
-              </div>
-              <div className="links">
-                <div className="link">
-                  <img src="images/library.png" alt=""/>
-                  <h2>Aristotle</h2>
-                </div>
-                <div className="link">
-                  <img src="images/library.png" alt=""/>
-                  <h2>Herodotus</h2>
-                </div>
-                <div className="link">
-                  <img src="images/library.png" alt=""/>
-                  <h2>Xenophon</h2>
-                </div>
-                <div className="link">
-                  <img src="images/library.png" alt=""/>
-                  <h2>Hippocrates</h2>
-                </div>
-              </div>
-              <a href="https://github.com/QubitPi/alexandria" style={{textDecoration: "none"}}>
-                <div className="github">
-                  <h2>Fork it on GitHub</h2>
-                  <img src="images/github.png" width="80%" alt=""/>
-                </div>
-              </a>
-
-            </div>
+            <Dashboard />
             <div className="audios">
               <div className="status">
                 <h1>Aristotle</h1>
---
+++ src/components/dashboard/Dashboard.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Dashboard.css';
+
+function Dashboard() {
+  return (
+      <div className="dashboard">
+        <div className="project">
+          <div className="imageCropper">
+            <img src="./images/avatar.png" alt=""/>
+          </div>
+          <h3>Alexandria</h3>
+          <p>Library</p>
+        </div>
+        <div className="links">
+          <div className="link">
+            <img src="images/library.png" alt=""/>
+            <h2>Aristotle</h2>
+          </div>
+          <div className="link">
+            <img src="images/library.png" alt=""/>
+            <h2>Herodotus</h2>
+          </div>
+          <div className="link">
+            <img src="images/library.png" alt=""/>
+            <h2>Xenophon</h2>
+          </div>
+          <div className="link">
+            <img src="images/library.png" alt=""/>
+            <h2>Hippocrates</h2>
+          </div>
+        </div>
+        <a href="https://github.com/QubitPi/alexandria" style={{textDecoration: "none"}}>
+          <div className="github">
+            <h2>Fork it on GitHub</h2>
+            <img src="images/github.png" width="80%" alt=""/>
+          </div>
+        </a>
+
+      </div>
+  );
+}
+
+export default Dashboard;
